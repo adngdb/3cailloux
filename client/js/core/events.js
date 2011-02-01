65917027f7f607557eb665ca17c02ee2f05e3b56
AdrianGaudebert <adrian@gaudebert.fr>
2011-02-01 15:53:35 +0100

    Handling the createGame Button

--- client/js/core/events.js
+++ client/js/core/events.js
@@ -1,11 +1,15 @@
-function Events(game) {
-    this.game = game;
+function Events(engine) {
+    this.engine = engine;
     this.bound = $.browser == 'msie' ? '#map' : window;
+
+    this.createGameBtn = 'button#create-game';
 }
 
 Events.prototype = {
     bindAll: function() {
+        log('Events.bindAll');
         this.bindKeys();
+        this.bindCreateGame();
         return this;
     },
 
@@ -13,11 +17,21 @@ Events.prototype = {
         var instance = this;
         $(this.bound).keypress(function(e) {
             switch(e.charCode || e.keyCode) {
-                case 74: case 106: instance.game.moveLeft(); break; // J
-                case 76: case 108: instance.game.moveRight(); break; // L
-                case 73: case 105: instance.game.changeShape(); break; // I
+                case 74: case 106: instance.engine.moveLeft(); break; // J
+                case 76: case 108: instance.engine.moveRight(); break; // L
+                case 73: case 105: instance.engine.changeShape(); break; // I
             }
             return false;
         });
-    }
+        return this;
+    },
+
+    bindCreateGame: function() {
+        var me = this;
+        $(this.createGameBtn).click(function(e) {
+            e.preventDefault();
+            me.engine.createGame();
+        });
+        return this;
+    },
 }
